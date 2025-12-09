27389427f090a98d8cec48f9ce9276bc07c16d4f
neves350 <martimneves350@gmail.com>
2025-12-09 14:48:05 +0000

    installation and setup about tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('todo-app-angular20');
+	protected readonly title = signal('todo-app-angular20')
 }

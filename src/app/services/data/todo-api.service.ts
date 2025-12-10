64427be64de0925f582c34a1f124c42306ebd9e0
neves350 <martimneves350@gmail.com>
2025-12-10 19:39:13 +0000

    implemented todo api service

--- src/app/services/data/todo-api.service.ts
+++ src/app/services/data/todo-api.service.ts
@@ -1,6 +1,41 @@
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { inject, Injectable } from '@angular/core'
+import type { Filter } from '../../models/filters.model'
+import type { Observable } from 'rxjs'
+import type { Todo } from '../../models/todo.model'
+import { environment } from '../../../environments/environment'
 
 @Injectable({
 	providedIn: 'root',
 })
-export class TodoApiService {}
+export class TodoApiService {
+	private readonly http = inject(HttpClient)
+	private readonly baseUrl = '/todos'
+
+	listTodos(filter: Filter = 'all'): Observable<Todo[]> {
+		const url = `${environment.apiUrl}${this.baseUrl}?filter=${filter}`
+
+		return this.http.get<Todo[]>(url)
+	}
+
+	createTodo(title: string): Observable<Todo> {
+		const url = `${environment.apiUrl}${this.baseUrl}`
+
+		return this.http.post<Todo>(url, { title })
+	}
+
+	updateTodo(
+		id: string,
+		payload: Partial<Pick<Todo, 'title' | 'completed'>>,
+	): Observable<Todo> {
+		const url = `${environment.apiUrl}${this.baseUrl}/${id}`
+
+		return this.http.put<Todo>(url, payload)
+	}
+
+	deleteTodo(id: string): Observable<void> {
+		const url = `${environment.apiUrl}${this.baseUrl}/${id}`
+
+		return this.http.delete<void>(url)
+	}
+}
